c5996c8f418a561861301565248cb269b70bb395
juani <juangarnero@hotmail.com.ar>
2023-01-13 13:29:15 -0300

    nav component has been finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
